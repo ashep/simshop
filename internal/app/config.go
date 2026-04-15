06572a671d8703f69e1c0936e185bb65928270b3
Oleksandr Shepetko <a@shepetko.com>
2026-04-15 20:55:43 +0300

    feat: add GoogleSheetsConfig and POST /orders OpenAPI spec

--- internal/app/config.go
+++ internal/app/config.go
@@ -9,9 +9,17 @@ type NovaPoshtaConfig struct {
 	ServiceURL string `yaml:"service_url"` // overridden in tests; empty means use production URL
 }
 
+type GoogleSheetsConfig struct {
+	CredentialsJSON string `yaml:"credentials_json"`
+	SpreadsheetID   string `yaml:"spreadsheet_id"`
+	SheetName       string `yaml:"sheet_name"`
+	ServiceURL      string `yaml:"service_url"` // overridden in tests; empty means use production URL
+}
+
 type Config struct {
-	Debug      bool            `yaml:"debug"`
-	Server     Server          `yaml:"server"`
-	DataDir    string          `yaml:"data_dir"`
-	NovaPoshta NovaPoshtaConfig `yaml:"nova_poshta"`
+	Debug        bool               `yaml:"debug"`
+	Server       Server             `yaml:"server"`
+	DataDir      string             `yaml:"data_dir"`
+	NovaPoshta   NovaPoshtaConfig   `yaml:"nova_poshta"`
+	GoogleSheets GoogleSheetsConfig `yaml:"google_sheets"`
 }
