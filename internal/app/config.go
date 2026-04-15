31967ea61f85241d6610e24d13a1559889a5ab52
Oleksandr Shepetko <a@shepetko.com>
2026-04-15 16:56:57 +0300

    feat: add NovaPoshtaConfig to app config

--- internal/app/config.go
+++ internal/app/config.go
@@ -4,8 +4,14 @@ type Server struct {
 	Addr string `yaml:"addr"`
 }
 
+type NovaPoshtaConfig struct {
+	APIKey     string `yaml:"api_key"`
+	ServiceURL string `yaml:"service_url"` // overridden in tests; empty means use production URL
+}
+
 type Config struct {
-	Debug   bool   `yaml:"debug"`
-	Server  Server `yaml:"server"`
-	DataDir string `yaml:"data_dir"`
+	Debug      bool            `yaml:"debug"`
+	Server     Server          `yaml:"server"`
+	DataDir    string          `yaml:"data_dir"`
+	NovaPoshta NovaPoshtaConfig `yaml:"nova_poshta"`
 }
